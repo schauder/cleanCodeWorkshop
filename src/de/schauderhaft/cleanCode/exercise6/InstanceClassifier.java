5d40a95dbddab2418c237b1c91cb0adcc4194929
Jens Schauder <jens.schauder@freenet.de>
2013-08-11 12:44:53 +0200

    refactored InstanceClassifier

--- src/de/schauderhaft/cleanCode/exercise6/InstanceClassifier.java
+++ src/de/schauderhaft/cleanCode/exercise6/InstanceClassifier.java
@@ -1,19 +1,46 @@
 package de.schauderhaft.cleanCode.exercise6;
 
+import static java.util.Arrays.asList;
+import static java.util.Collections.unmodifiableSet;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import de.schauderhaft.cleanCode.stubs.SrvParameterCache;
+
 public class InstanceClassifier {
 
-    boolean isTestInstance(String url) {
-        return url.contains("grglmrpft") || url.contains("sysmrpt")
-                || url.contains("gru23b12") || url.contains("gru23b13");
+    private final Set<String> testInstances = unmodifiableSet(new HashSet<String>(
+            asList("grglmrpft", "sysmrpt", "gru23b12", "gru23b13")));
+    private final Set<String> qaInstances = unmodifiableSet(new HashSet<String>(
+            asList("grglmrpfqs", "gru23c12", "sysglmqs", "sysgmrpf-qs")));
+
+    boolean isTestInstance() {
+        return matchesAny(sanatizedServerUrl(), testInstances);
     }
 
-    boolean isQaInstance(String url) {
-        return url.contains("grglmrpfqs") || url.contains("gru23c12")
-                || url.contains("sysglmqs") || url.contains("sysgmrpf-qs");
+    boolean isQaInstance() {
+        return matchesAny(sanatizedServerUrl(), qaInstances);
     }
 
-    boolean isProductionInstance(String url) {
-        return !(isTestInstance(url) || isQaInstance(url));
+    boolean isProductionInstance() {
+        return !(isTestInstance() || isQaInstance());
     }
 
+    private boolean matchesAny(String sanitizedUrl, Set<String> instances) {
+        for (String pattern : instances) {
+            if (sanitizedUrl.contains(pattern))
+                return true;
+        }
+        return false;
+    }
+
+    private String sanatizedServerUrl() {
+        String url = SrvParameterCache
+                .getParameterAsString(SrvParameterCache.WEB_URL_DEFAULT);
+        if (url == null)
+            url = "";
+
+        return url.toLowerCase();
+    }
 }

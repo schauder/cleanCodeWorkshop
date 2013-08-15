c8cf1ee7a9a1f396679e40d6a68db40ae20eb4f2
René Pfeuffer <rene@pfeuffer-web.de>
2013-08-15 21:12:15 +0200

    no need to keep the instances in a set in InstanceClassifier

--- src/de/schauderhaft/cleanCode/exercise6/InstanceClassifier.java
+++ src/de/schauderhaft/cleanCode/exercise6/InstanceClassifier.java
@@ -1,19 +1,17 @@
 package de.schauderhaft.cleanCode.exercise6;
 
 import static java.util.Arrays.asList;
-import static java.util.Collections.unmodifiableSet;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Collection;
 
 import de.schauderhaft.cleanCode.stubs.SrvParameterCache;
 
 public class InstanceClassifier {
 
-    private final Set<String> testInstances = unmodifiableSet(new HashSet<String>(
-            asList("grglmrpft", "sysmrpt", "gru23b12", "gru23b13")));
-    private final Set<String> qaInstances = unmodifiableSet(new HashSet<String>(
-            asList("grglmrpfqs", "gru23c12", "sysglmqs", "sysgmrpf-qs")));
+    private final Collection<String> testInstances = asList("grglmrpft",
+            "sysmrpt", "gru23b12", "gru23b13");
+    private final Collection<String> qaInstances = asList("grglmrpfqs",
+            "gru23c12", "sysglmqs", "sysgmrpf-qs");
 
     boolean isTestInstance() {
         return matchesAny(sanatizedServerUrl(), testInstances);
@@ -27,7 +25,7 @@ public class InstanceClassifier {
         return !(isTestInstance() || isQaInstance());
     }
 
-    private boolean matchesAny(String sanitizedUrl, Set<String> instances) {
+    private boolean matchesAny(String sanitizedUrl, Collection<String> instances) {
         for (String pattern : instances) {
             if (sanitizedUrl.contains(pattern))
                 return true;
